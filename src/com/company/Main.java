f5014c0baaa429047e469b8a570cc0872797f1e1
macnailyat <macnailyat@gmail.com>
2021-11-06 10:56:30 -0400

    Solar System Basic Strucuture is Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem andromeda = new SolarSystem();
+        andromeda.planet = 2;
+        andromeda.moon = 4;
+        andromeda.Sun = 1;
+    }
+}
